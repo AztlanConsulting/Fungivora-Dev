81767a928653ade8276b1788bdafd1ccfd4242a5
Gamezas <elgamezas@gmail.com>
2026-04-08 10:13:42 -0600

    [ADD] (frontend) Added files to start working navbar

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+/*
+Esta es la configuración para el componente de la navbar
+*/
+
+function Navbar() {
+    <aside className="h-screen">
+        <nav>
+
+        </nav>
+    </aside>
+}
