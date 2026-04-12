1f5522d7c51827434a01aa70ee34aa89d4236f88
Gamezas <elgamezas@gmail.com>
2026-04-12 13:06:55 -0600

    [UPDATE] (frontend) Corrections for mobile view

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,29 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import { HugeiconsIcon } from '@hugeicons/react';
 import { Home07FreeIcons, BookOpenTextFreeIcons, PackageIcon, MushroomIcon, Notification01Icon, Logout02Icon, NaturalFoodIcon, } from '@hugeicons/core-free-icons';
 import fungivora from "/icons/icon-512x512.png?url"
 import '../styles/navbar.css'
 
 export default function MainLayout () {
+    const [iconSize, setIconSize] = useState(window.innerWidth < 768 ? 28 : 40);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 768) {
+                setIconSize(30);
+            }
+            else {
+                setIconSize(40);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+
+    }, []);
+
     return (
         <div className="layout">
             <aside>
@@ -13,79 +32,59 @@ export default function MainLayout () {
                         
                     <div className="contenedor-principal-nav">
                         <NavLink
-                            to="/second"
+                            to="/first"
                             className={({ isActive }) => 
                                 `botones p-2 self-center rounded-lg transition-colors ${isActive ? "bg-blue-100" : "hover:bg-gray-100"}`
                             }>
-                            <HugeiconsIcon icon={Home07FreeIcons} size={40} color="#3b3fb6" strokeWidth={1.5} />
+                            <HugeiconsIcon icon={Home07FreeIcons} size={iconSize} color="#3b3fb6" strokeWidth={1.5} />
                             <span className="nav-tooltip">
                                 Inicio
                             </span>
                         </NavLink>
 
                         <NavLink 
-                            to="/second"
+                            to="/first"
                             className={({ isActive }) => 
                                 `botones p-2 self-center rounded-lg transition-colors ${isActive ? "bg-blue-100" : "hover:bg-gray-100"}`
                             }> 
-                            <HugeiconsIcon icon={BookOpenTextFreeIcons} size={40} color="#3b3fb6" strokeWidth={1.5} />
+                            <HugeiconsIcon icon={BookOpenTextFreeIcons} size={iconSize} color="#3b3fb6" strokeWidth={1.5} />
                             <span className="nav-tooltip">
                                 Experimentos
                             </span>
                         </NavLink>
 
                         <NavLink 
-                            to="/second"
+                            to="/inventario"
                             className={({ isActive }) => 
                                 `botones p-2 self-center rounded-lg transition-colors ${isActive ? "bg-blue-100" : "hover:bg-gray-100"}`
                             }> 
-                            <HugeiconsIcon icon={PackageIcon} size={40} color="#3b3fb6" strokeWidth={1.5} />
+                            <HugeiconsIcon icon={PackageIcon} size={iconSize} color="#3b3fb6" strokeWidth={1.5} />
                             <span className="nav-tooltip">
                                 Invetario
                             </span>
                         </NavLink>
 
                         <NavLink
-                            to="/second"
+                            to="/first"
                             className={({ isActive }) => 
                                 `botones p-2 self-center rounded-lg transition-colors ${isActive ? "bg-blue-100" : "hover:bg-gray-100"}`
                             }> 
-                            <HugeiconsIcon icon={MushroomIcon} size={40} color="#3b3fb6" strokeWidth={1.5} />
+                            <HugeiconsIcon icon={MushroomIcon} size={iconSize} color="#3b3fb6" strokeWidth={1.5} />
                             <span className="nav-tooltip">
                                 Recetario
                             </span>
                         </NavLink>
 
                         <NavLink
-                            to="/second"
+                            to="/first"
                             className={({ isActive }) => 
                                 `botones p-2 self-center rounded-lg transition-colors ${isActive ? "bg-blue-100" : "hover:bg-gray-100"}`
                             }> 
-                            <HugeiconsIcon icon={NaturalFoodIcon} size={40} color="#3b3fb6" strokeWidth={1.5} />
+                            <HugeiconsIcon icon={NaturalFoodIcon} size={iconSize} color="#3b3fb6" strokeWidth={1.5} />
                             <span className="nav-tooltip">
                                 Lotes
                             </span>
                         </NavLink>
-                    </div>
-
-                    <div className="contenedor-inferior-nav">
-                        <NavLink 
-                            to="/second"
-                            className={({ isActive }) => 
-                                `botones p-2 self-center rounded-lg transition-colors ${isActive ? "bg-blue-100" : "hover:bg-gray-100"}`
-                            }>
-                            <HugeiconsIcon icon={Notification01Icon} size={40} color="#3b3fb6" strokeWidth={1.5} />
-                            <span className="nav-tooltip">
-                                Notificaciones
-                            </span>
-                        </NavLink>
-
-                        <button className="botones flex p-2 self-center rounded-lg transition-colors hover:bg-gray-100">
-                            <HugeiconsIcon icon={Logout02Icon} size={40} color="#3b3fb6" strokeWidth={1.5} />
-                            <span className="nav-tooltip">
-                                Cerrar sesión
-                            </span>
-                        </button>
                     </div>
                 </nav> 
             </aside>
