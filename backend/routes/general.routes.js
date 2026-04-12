ec10c19b12eb9fdbcbf766f70521f3f997112566
A01712317 <A01712317@tec.mx>
2026-04-12 09:57:22 -0600

    [ADD](backend) endpont consult categories

--- backend/routes/general.routes.js
+++ backend/routes/general.routes.js
@@ -5,10 +5,14 @@ const router = express.Router();
 // Inicio de sesión
 const loginRoutes = require('./inicio_sesion.routes');
 const pruebaRoutes = require('./prueba_db.routes');
+// Inicio de sesión
+const insumosRoutes = require('./insumos.routes');
 
 // Prefijos de rutas
 // Inicio de sesión
 router.use('/login', loginRoutes);
 router.use('/prueba', pruebaRoutes);
+router.use('/insumos', insumosRoutes);
+
 
 module.exports = router;
