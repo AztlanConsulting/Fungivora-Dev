bcc351387aa3ee3b826a08116383d50e52293c83
EmiliaCardenas <a01706807@tec.mx>
2026-04-25 08:57:09 -0600

    [ADD] (frontend) addign title box

--- frontend/src/componentes/titulo.jsx
+++ frontend/src/componentes/titulo.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Text from "./texto";
 
 const Titulo = ({ children }) => {
-    return (
-    <h1 className="text-4xl md:text-6xl font-bold text-[#3b3fb6] mb-8 md:mb-10 text-center md:text-left">
-        {children}
-    </h1>)};
+  return (
+    <div
+    className="
+        w-screen  
+        absolute top-0 left-0 
+        bg-[#3b3fb6]
+        shadow-[0_10px_25px_rgba(0,0,0,0.4)]
+        flex items-left
+        h-auto 
+        px-8 py-8        
+        md:px-12 md:py-6  
+    "
+    >
+      <div className="w-full">
+        <Text variante="title">
+          {children}
+        </Text>
+      </div>
+    </div>
+  );
+};
 
 export default Titulo;
--- frontend/src/views/pruba_componentes.jsx
+++ frontend/src/views/pruba_componentes.jsx
@@ -1,37 +1,15 @@
 import React from "react";
 import Base from "../componentes/base";
-import Text from "../componentes/texto";
+import Titulo from "../componentes/titulo";
 
 function Pruebas() {
   return (
-    <Base margen_arriba="mt-8 md:mt-[vh]">
-      <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+    <Base margen_arriba="mt-8 md:mt-[10vh]">
+      <div style={{ padding: "20px" }}>
         
-        <Text variante="title">Título</Text>
-
-        <Text variante="popup">Texto Pop Up</Text>
-
-        <Text variante="medium">Texto Mediano</Text>
-
-        <Text variante="label">Texto del select...</Text>
-
-        <Text variante="label">Texto descripción de Input</Text>
-
-        <Text variante="input">Texto número input</Text>
-
-        <Text variante="button">Texto Botón</Text>
-
-        <Text variante="dates">Texto Fechas</Text>
-
-        <Text variante="placeholder">
-          Texto Input ejemplo...
-        </Text>
-
-        <Text variante="option">Texto Opción</Text>
-
-        <Text variante="selected">
-          Texto Opción Seleccionada
-        </Text>
+        <Titulo>
+          Titulo.... prueba prueba prueba
+        </Titulo>
 
       </div>
     </Base>
