13cae370fd2745839755fafcadb6b8078a952c56
Rigby119 <rigby119@gmail.com>
2026-04-28 15:11:23 -0600

    [UPDATE] (backend) General route update to /api

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const { register } = require('./config/metrics');
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/status', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({ mensaje: "Backend operativo y conectado a Nginx" });
 });
 
@@ -22,7 +22,7 @@ app.get('/metrics', async (req, res) => {
     res.end(await register.metrics());
 });
 
-app.use('/', generalRoutes);
+app.use('/api', generalRoutes);
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
