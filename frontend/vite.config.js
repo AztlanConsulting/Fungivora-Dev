0346f9db45ae38551c1d6b3f36f5b6e323324989
Rigby119 <rigby119@gmail.com>
2026-04-28 15:11:56 -0600

    [UPDATE] (frontend) Vite Config for Proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -40,6 +40,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['localhost']
+    allowedHosts: ['localhost'],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
