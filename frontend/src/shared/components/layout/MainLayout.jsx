5acaa9f523e8cfa790c66d50a49559df4dccbf11
Rigby119 <rigby119@gmail.com>
2026-04-28 22:39:46 -0600

    [FIX] (frontend) MainLayout import correction

--- frontend/src/shared/components/layout/MainLayout.jsx
+++ frontend/src/shared/components/layout/MainLayout.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Barra_navegacion from "../componentes/barra_navegacion";
+import { BarraBusqueda } from "../ui";
 import { Outlet } from "react-router-dom";
 
-export default function MainLayout () {
+export default function MainLayout() {
     return (
         <div className="flex flex-col-reverse min-h-dvh md:flex-row">
-            <Barra_navegacion />
+            <BarraBusqueda />
 
             {/* //! Outlet se encarga de renderizar la vista sin desaparecer la barra de navegacion */}
             <main className="flex-1">
