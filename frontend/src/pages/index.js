fce4ca002139bd8af28fb400c2687f575c88ccfd
Gamezas <elgamezas@gmail.com>
2026-04-30 16:06:24 -0600

    [UPDATE] (frontend) Routing and dummy pages to test NavLink routing

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -7,3 +7,5 @@ export { default as Lotes } from "./lotes/LotesView";
 export { default as Pruebas } from "./tests/pruba_componentes";
 export { default as Usuario } from "./users/Usuario";
 export { default as Login } from "./users/login";
+export { default as BibliotecaGenetica } from "./inoculos/biblioteca_genetica";
+export { default as InventarioPrueba } from "./inventario/inventariodummy"
