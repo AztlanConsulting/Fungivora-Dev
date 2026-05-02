c298c2938674bb14819af7ca89eac6e01c577164
Yugior <horasvihe@gmail.com>
2026-05-02 14:11:27 -0600

    [UPDATE] (comments) A title in component

--- frontend/src/features/inoculos/components/selecionar_especie.jsx
+++ frontend/src/features/inoculos/components/selecionar_especie.jsx
@@ -41,47 +41,54 @@ const SelectEspecie = ({
     numero: "w-24 md:w-32",
   };
 
-  // Color del texto: gris si no hay valor seleccionado, azul si hay valor
   const textColor = value ? colores.azul : colores.gris;
 
   const clase = `${sizes[size]} 
     border-2 
     rounded-xl 
     px-3 py-2 pr-8 
-    text-sm
+    text-base
     outline-none cursor-pointer appearance-none 
     transition-colors focus:border-[#3b3fb6]`;
 
   return (
-    <div className="relative w-fit">
-      <select
-        value={value}
-        onChange={onChange}
-        className={clase}
-        style={{
-          backgroundColor: "#F9FDFF",
-          color: textColor,
-          borderColor: colores.grisMedio,
-        }}
-        disabled={loading}
-      >
-        <option value="" disabled hidden>
-          {loading ? "Cargando especies..." : error ? error : placeholder}
-        </option>
+    <div className="flex flex-col gap-2">
 
-        {especies.map((esp) => (
-          <option key={esp.especie} value={esp.especie}>
-            {esp.especie}
-          </option>
-        ))}
-      </select>
-
-      <span
-        className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2"
-        style={{ color: colores.azul }}
-      >
-        <HugeiconsIcon icon={ArrowDown01Icon} size={25} />
+      {/* Título del componente */}
+      <span style={{ fontSize: "28px", color: colores.negro, fontWeight: "500" }}>
+        Especie
       </span>
+
+      <div className="relative w-fit">
+        <select
+          value={value}
+          onChange={onChange}
+          className={clase}
+          style={{
+            backgroundColor: "#F9FDFF",
+            color: textColor,
+            borderColor: colores.grisMedio,
+          }}
+          disabled={loading}
+        >
+          <option value="" disabled hidden>
+            {loading ? "Cargando especies..." : error ? error : placeholder}
+          </option>
+
+          {especies.map((esp) => (
+            <option key={esp.especie} value={esp.especie}>
+              {esp.especie}
+            </option>
+          ))}
+        </select>
+
+        <span
+          className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2"
+          style={{ color: colores.azul }}
+        >
+          <HugeiconsIcon icon={ArrowDown01Icon} size={25} />
+        </span>
+      </div>
     </div>
   );
 };
