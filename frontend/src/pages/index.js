fab571413554a2f81746605eb983da3ae7c95411
Gamezas <elgamezas@gmail.com>
2026-05-02 18:01:56 -0600

    [FIX] (frontend) Export not working correctly

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,4 +8,5 @@ export { default as Pruebas } from "./tests/pruba_componentes";
 export { default as Usuario } from "./users/Usuario";
 export { default as Login } from "./users/login";
 export { default as BibliotecaGenetica } from "./inoculos/biblioteca_genetica";
-export { default as InventarioPrueba } from "./inventario/inventariodummy"export { default as PruebaDb } from "./tests/PruebaDb";
+export { default as InventarioPrueba } from "./inventario/inventariodummy";
+export { default as PruebaDb } from "./tests/PruebaDb";
