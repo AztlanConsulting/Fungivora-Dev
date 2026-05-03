16dd9d4cfed8cdca492ceedb0986df2bdc49ee99
Yugior <horasvihe@gmail.com>
2026-05-03 10:15:41 -0600

    [UPDATE] (frontend/pages) Update pages index exports

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,5 +8,5 @@ export { default as Pruebas } from "./tests/pruba_componentes";
 export { default as Usuario } from "./users/Usuario";
 export { default as Login } from "./users/login";
 export { default as BibliotecaGenetica } from "./inoculos/biblioteca_genetica";
-export { default as InventarioPrueba } from "./inventario/inventariodummy";
+export { default as InventarioPrueba } from "./inventario/inventario";
 export { default as PruebaDb } from "./tests/PruebaDb";
