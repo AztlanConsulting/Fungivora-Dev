72d9b79749ba5282c04283c5ea3994811a2f8b72
Rigby119 <rigby119@gmail.com>
2026-05-09 19:17:21 -0600

    [UPDATE] (frontend) Pages index updated

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,6 +3,7 @@ export { default as RegistrarMedio } from "./inoculos/NewMedio";
 export { default as Inventario } from "./inventario/inventario";
 export { default as RegistrarInsumo } from "./inventario/RegistrarInsumo";
 export { default as Lotes } from "./lotes/Lotes";
+export { default as Lote } from "./lotes/Lote";
 export { default as Pruebas } from "./tests/pruba_componentes";
 export { default as Usuario } from "./users/Usuario";
 export { default as Login } from "./users/login";
