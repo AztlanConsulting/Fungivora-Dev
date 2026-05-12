9f084587a556f7165d46a5d4d8a1f345700e511c
Rigby119 <rigby119@gmail.com>
2026-05-12 00:32:33 -0600

    [FIX] (backend) Improved MySQL pool

--- backend/util/db.js
+++ backend/util/db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const path = require('path');
 
-dotenv.config({ path: require('path').join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -9,7 +10,17 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  connectionLimit: 10
+
+  waitForConnections: true,
+  connectionLimit: 15,
+  queueLimit: 0,
+
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+});
+
+pool.on('error', (err) => {
+  console.error('MySQL Pool Error:', err);
 });
 
 module.exports = pool.promise();
