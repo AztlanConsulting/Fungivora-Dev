bd22f5bca037f29d7e1a2bd4e271d6af33424f69
Marel52 <A01424302@tec.mx>
2026-05-15 12:44:44 -0600

    [DELETE] (frontend) Deletes refs to deleted NewMedio page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,8 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import {
     FirstPage, Login, Usuario, Inventario,
-     RegistrarMedio, Lotes,
-    Lote, BibliotecaGenetica
+    Lotes, Lote, BibliotecaGenetica
 } from "../pages";
 import { MainLayout, RutaProtegida } from "../shared/components/layout";
 import FormAgar from "../pages/inoculos/agar/Formagar";
@@ -33,7 +32,6 @@ export default function AppRoutes() {
 
                 {/* Rutas inventario */}
                 <Route path="/inventario" element={<Inventario />} />
-                <Route path="/inventario/micelio/crear" element={<RegistrarMedio />} />
 
                 {/* Rutas biblioteca genetica */}
                 <Route path="/inoculos" element={<BibliotecaGenetica />} />
