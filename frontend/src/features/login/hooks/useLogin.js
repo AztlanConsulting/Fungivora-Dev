c351203b450842c68e3dac2955bf5f808b431700
Rigby119 <rigby119@gmail.com>
2026-05-22 00:59:19 -0600

    [FIX] (frontend) Minimal fix to frontend import and handling

--- frontend/src/features/login/hooks/useLogin.js
+++ frontend/src/features/login/hooks/useLogin.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import loginService from "../services/login.service";
+import loginService from "../service/login.service";
 
 const useLogin = () => {
     const [cargando, setCargando] = useState(false);
@@ -11,17 +11,17 @@ const useLogin = () => {
         try {
             const data = await loginService.login(usuario, password);
             localStorage.setItem("token", data.token); //guarda localmente el token
-            return data; 
+            return data;
         } catch (err) {
             const mensaje = err.response?.data?.msg || "Usuario y/o contraseña incorrectos"; //Mensaje de error de usuario y/o contraseña
             setError(mensaje);
-            throw err; 
+            throw err;
         } finally {
             setCargando(false);
         }
     }, []);
 
-    return { ejecutarLogin: login, cargando, error }; 
+    return { ejecutarLogin: login, cargando, error };
 }
 
 export default useLogin;
