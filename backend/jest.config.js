b3b3088b91013ecb6e1371447312add589c59921
EmiliaCardenas <a01706807@tec.mx>
2026-06-04 21:39:21 -0600

    [UPDATE] (backend) adding more tiem

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     testEnvironment: 'node',
-    testTimeout: 10000,
+    testTimeout: 15000,
     testMatch: ['**/test/**/*.test.js'],
     collectCoverageFrom: [
         'controllers/**/*.js',
