5529c4c5e1b68d76d5caeca19c151f3cef322644
Marel52 <A01424302@tec.mx>
2026-06-05 17:07:35 -0600

    [FIX] (frontend) 'Ver Bloques' button no loger covers the other buttons

--- frontend/src/pages/lotes/Lotes.jsx
+++ frontend/src/pages/lotes/Lotes.jsx
@@ -333,7 +333,7 @@ const [todosLosLotesMarcados, setTodosLosLotesMarcados] = useState(false);
         />
       </div>
 
-      <div className="flex flex-col min-[1450px]:flex-row gap-8 items-center min-[1450px]:items-start min-h-[60vh]">
+      <div className="flex flex-col min-[1450px]:flex-row gap-8 items-center min-[1450px]:items-start min-h-[60vh] pb-32 min-[1450px]:pb-0">
         {/* Componente de las tablas*/}
       <div className={`w-full min-[1450px]:flex-1 bg-white rounded-[32px] shadow-sm border p-4 md:p-8 ${verFormulario ? "hidden" : "block"} min-[1450px]:block`}>
         {paso === 1 ? (
